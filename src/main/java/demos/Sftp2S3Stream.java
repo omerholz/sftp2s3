7b018fcd45c28d09edcebe60a8ee1f80d79ca7cb
omerholz <omer.holz@gmail.com>
2018-01-08 14:31:55 -0500

    log sink

--- src/main/java/demos/Sftp2S3Stream.java
+++ src/main/java/demos/Sftp2S3Stream.java
@@ -1,7 +1,7 @@
 package demos;
 
 import org.springframework.cloud.stream.app.sftp.source.SftpSourceConfiguration;
-import org.springframework.cloud.stream.app.s3.sink.AmazonS3SinkConfiguration;
+import org.springframework.cloud.stream.app.log.sink.LogSinkConfiguration;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.aggregate.AggregateApplicationBuilder;
@@ -12,7 +12,6 @@ public class  Sftp2S3Stream {
 	public static void main(String[] args) {
 		new AggregateApplicationBuilder(Sftp2S3Stream.class, args)
 				.from(SftpSourceConfiguration.class).namespace("sftp").args("--mode=contents")
-				.to(AmazonS3SinkConfiguration.class).namespace("s3").args().run(args);
+				.to(LogSinkConfiguration.class).namespace("log").args("--level=debug --name=omer").run(args);
 	}
-
 }
